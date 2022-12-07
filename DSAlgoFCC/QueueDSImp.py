55679bf85379c34d90b4a4aa572ebba4110d8b26
bsuman <tsui.champ@gmail.com>
2022-12-07 11:00:31 +0100

    Stack and queue implementation added

---
+++ DSAlgoFCC/QueueDSImp.py
@@ -0,0 +1,98 @@
+# Implementation of queue data structure ( LiFo)
+# 1. using lists
+# 2. using linked list
+
+# supporting functions:
+# remove: removes the first item in the queue
+# add: adds the item to the end of the queue
+# peek: reads the first item in the queue and return the item
+# isEmpty: function to check if the queue is empty
+
+class myqueue:
+    def __init__(self):
+        self.data = []
+
+    def add(self, item):
+        self.data.append(item)
+
+    def remove(self):
+        if not self.isEmpty():
+            self.data.pop(0)
+
+    def peek(self):
+        if not self.isEmpty():
+            return self.data[0]
+
+    def isEmpty(self):
+        return len(self.data) == 0
+
+
+class node:
+    def __init__(self, idata):
+        self.data = idata
+        self.next = None
+
+class myqueuell:
+    def __init__(self):
+        self.first = None
+        self.last = None
+
+    def add(self, item):
+        if self.first is None:
+            self.first = item
+            self.last = self.first
+        else:
+            self.last.next = item
+            self.last = item
+
+    def remove(self):
+        if self.first is None:
+            return "Queue Empty!"
+        elif self.first == self.last:
+            self.first = None
+            self.last = None
+        else:
+            self.first = self.first.next
+
+    def peek(self):
+        if self.first is None:
+            return "Queue Empty!"
+        else:
+            return self.first.data
+
+    def isEmpty(self):
+        return self.first is None and self.last is None
+
+
+
+if __name__ == "__main__":
+    mq = myqueue()
+    print(mq.isEmpty())
+    mq.add(4)
+    mq.add(5)
+    mq.add(6)
+    print(mq.isEmpty())
+    print(mq.peek())
+    mq.remove()
+    print(mq.peek())
+    print(mq.isEmpty())
+    mq.remove()
+    mq.remove()
+    print(mq.isEmpty())
+    print("================")
+    mql = myqueuell()
+    print(mql.isEmpty())
+    mql.add(node(4))
+    mql.add(node(5))
+    mql.add(node(6))
+    print(mql.isEmpty())
+    print(mql.peek())
+    mql.remove()
+    print(mql.peek())
+    mql.add(node(8))
+    print(mql.isEmpty())
+    mql.remove()
+    print(mql.peek())
+    mql.remove()
+    print(mql.peek())
+    print(mql.isEmpty())
---
+++ DSAlgoFCC/StackDSImp.py
@@ -0,0 +1,119 @@
+# Stack implementation
+# 1. using lists
+# 2. using linked list
+
+# supporting functions:
+# pop: removes the first item in the stack
+# push: adds the item to the top of the stack
+# peek: reads the element from the top of the stack and return the element
+# isEmpty: function to check if the stack is empty
+class StackEmptyError(Exception):
+    pass
+
+
+class Mystack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.insert(0, item)
+        print("Item = {} pushed to the stack".format(item))
+
+    def pop(self):
+        try:
+            return self.stack.pop(0)
+        except:
+            print("Stack is empty!")
+
+    def peek(self):
+        try:
+            return self.stack[0]
+        except:
+            print("Stack is empty!")
+
+    def isEmpty(self):
+        return len(self.stack) == 0
+
+
+# ===========================================================================================================
+class stacknode:
+    def __init__(self, item):
+        self.data = item
+        self.next = None
+
+
+class mystackll:
+    def __init__(self):
+        self.top = None
+
+    def push(self, item):
+        if self.top is None:
+            self.top = item
+        else:
+            item.next = self.top
+            self.top = item
+
+    def pop(self):
+        try:
+            if self.top is None:
+                raise StackEmptyError
+            else:
+                data = self.top.data
+                if self.top.next is not None:
+                    self.top = self.top.next
+                else:
+                    self.top = None
+                return data
+        except StackEmptyError:
+            print("Stack is empty!")
+
+    def peek(self):
+        try:
+            if self.top:
+                return self.top.data
+            else:
+                raise StackEmptyError
+        except StackEmptyError:
+            print("Stack is empty!")
+
+    def isEmpty(self):
+        return self.top is None
+
+
+if __name__ == "__main__":
+    mstack = Mystack()
+    mstack.push(10)
+    mstack.push(20)
+    mstack.push(40)
+    mstack.push(30)
+    print(mstack.isEmpty())
+    print(mstack.peek())
+    print(mstack.pop())
+    print(mstack.peek())
+    print(mstack.pop())
+    print(mstack.peek())
+    print(mstack.pop())
+    print(mstack.pop())
+    print(mstack.peek())
+    print(mstack.isEmpty())
+
+    myllstack = mystackll()
+    node1 = stacknode("a")
+    myllstack.push(node1)
+    node1 = stacknode("b")
+    myllstack.push(node1)
+    node1 = stacknode("c")
+    myllstack.push(node1)
+    node1 = stacknode("d")
+    myllstack.push(node1)
+    print(myllstack.isEmpty())
+    print(myllstack.peek())
+    print(myllstack.pop())
+    print(myllstack.peek())
+    print(myllstack.pop())
+    print(myllstack.peek())
+    print(myllstack.pop())
+    print(myllstack.pop())
+    print(myllstack.peek())
+    print(myllstack.isEmpty())
