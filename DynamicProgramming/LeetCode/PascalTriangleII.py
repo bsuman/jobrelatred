a1202c95f2ceff6b03a9e743a946e93b6640749c
bsuman <tsui.champ@gmail.com>
2023-01-12 15:39:42 +0100

    solved a few problems

--- DynamicProgramming/LeetCode/PascalTriangleII.py
+++ DynamicProgramming/LeetCode/PascalTriangleII.py
@@ -2,24 +2,23 @@
 # In Pascal's triangle, each number is the sum of the two numbers directly above it as shown
 
 def getRow(rowIndex) -> list[int]:
-    dp = []
-    for i in range(0, rowIndex+1):
-        if i == 0:
-            dp.append([1])
-        elif i == 1:
-            dp.append([1, 1])
-        else:
-            numlist = dp[i - 1]
-            tmp = [1]
-            nln = len(numlist)
-            for j in range(nln):
-                if j + 1 < nln:
-                    num = numlist[j] + numlist[j + 1]
-                else:
-                    num = numlist[j]
-                tmp.append(num)
-            dp.append(tmp)
-    return dp[rowIndex]
+    if rowIndex == 0:
+        return [1]
+    elif rowIndex == 1:
+        return [1, 1]
+    numlist = [1, 1]
+    current = None
+    for i in range(2, rowIndex+1):
+        current = [1]
+        nln = len(numlist)
+        for j in range(nln):
+            if j + 1 < nln:
+                num = numlist[j] + numlist[j + 1]
+            else:
+                num = numlist[j]
+            current.append(num)
+        numlist = current
+    return current
 
 
 if __name__ == '__main__':
