ed11b038065686191ceffda6add4505d2169aeea
bsuman <tsui.champ@gmail.com>
2023-01-20 17:08:11 +0100

    solved a few problems

---
+++ DynamicProgramming/LeetCode/CoinChangeII.py
@@ -0,0 +1,18 @@
+# You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+# Return the number of combinations that make up that amount. If that amount of money cannot be made up by any combination of the coins, return 0.
+# You may assume that you have an infinite number of each kind of coin.
+# The answer is guaranteed to fit into a signed 32-bit integer.
+
+def change(amount: int, coins: list[int]) -> int:
+    dp = [0] * (amount + 1)
+    dp[0] = 1
+
+    for coin in coins:
+        for x in range(coin, amount + 1):
+            dp[x] += dp[x - coin]
+    return dp[amount]
+
+if __name__ == '__main__':
+    coins = [1, 2, 5]
+    amount = 5
+    print(change(amount,coins))
