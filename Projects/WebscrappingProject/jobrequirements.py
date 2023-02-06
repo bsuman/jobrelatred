bb2cdb0d4ec565229237fbdec29d392c63b490ab
bsuman <tsui.champ@gmail.com>
2023-02-06 11:57:48 +0100

    solved a few problems

--- Projects/WebscrappingProject/jobrequirements.py
+++ Projects/WebscrappingProject/jobrequirements.py
@@ -1,13 +1,20 @@
-from userinfo import access_token, return_url, key, secret
-from linkedin import linkedin
+import requests
+import csv
 
-authentication = linkedin.LinkedInDeveloperAuthentication(
-    key,
-    secret,
-    access_token,
-    return_url,
-    linkedin.PERMISSIONS.enums.values()
-)
-print(authentication.user_secret)
-application = linkedin.LinkedInApplication(authentication)
-print(application)
+def get_data():
+    data = requests.get(
+        "https://jobs.siemens.com/api/jobs?page=1&location=Deutschland&keywords=Python&sortBy=relevance&limit=100&stretch=0&stretchUnit=MILES&internal=false")
+
+    jresponse = data.json()
+
+    with open('job_scrape.csv', 'w', encoding="utf8") as csv_file:
+        csv_writer = csv.writer(csv_file, )
+        csv_writer.writerow(['Job_Title', 'Job_ID', 'City', 'Job_Type', 'Job_Level', 'Job_Description'])
+        print("Total number of positions for 'Python Developer Role' found at Siemens = {}".format(jresponse['count']))
+        for job in jresponse['jobs']:
+            jd = job['data']
+            py_lib = str(jd['description'])
+            csv_writer.writerow([jd['title'], jd['req_id'], jd['city'], jd['tags5'][0], jd['tags2'][0], py_lib])
+
+
+get_data()
