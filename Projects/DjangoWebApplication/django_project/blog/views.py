ffbae25ae370925ff6c801ac4ff8bcfed0b7b29d
bsuman <tsui.champ@gmail.com>
2023-02-09 15:53:33 +0100

    django project- blog post management web application

--- Projects/DjangoWebApplication/django_project/blog/views.py
+++ Projects/DjangoWebApplication/django_project/blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    DeleteView,
+    UpdateView)
 
 
 # Function to handle traffic from homepage to the blog
@@ -13,3 +20,53 @@ def home(request):
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-created_on']  # default is ascending but '-' infront changes it to descending
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        # get current post
+        post = self.get_object()
+        if post.author == self.request.user:
+            return True
+        else:
+            return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        # get current post
+        post = self.get_object()
+        if post.author == self.request.user:
+            return True
+        else:
+            return False
