ecf1e7eb4ebd48a02cb6d3e707ce2f891fa44f7c
bsuman <tsui.champ@gmail.com>
2023-02-09 10:37:03 +0100

    django project- blog post management web application

--- Projects/DjangoWebApplication/django_project/django_project/urls.py
+++ Projects/DjangoWebApplication/django_project/django_project/urls.py
@@ -18,6 +18,8 @@ from django.urls import path
 from django.urls import include
 from users import views as user_view
 from django.contrib.auth import views as auth_view
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +30,6 @@ urlpatterns = [
     # path('blog/', include('blog.urls')
     path('', include('blog.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
