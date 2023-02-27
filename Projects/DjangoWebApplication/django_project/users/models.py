4391f9abf491479c3ab85ca0a072bbcef6f31fb4
bsuman <tsui.champ@gmail.com>
2023-02-27 13:33:28 +0100

    django project- blog post management web application

--- Projects/DjangoWebApplication/django_project/users/models.py
+++ Projects/DjangoWebApplication/django_project/users/models.py
@@ -11,8 +11,8 @@ class Profile(models.Model):
     def __str__(self):
         return f"{self.user.username} Profile!"
 
-    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
-        super().save()
+    def save(self,  *args, **kwargs):
+        super().save(self, *args, **kwargs)
         img = Image.open(self.image.path)
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
